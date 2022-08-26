6f3ab13f3fb86ed4a51b012571aa7957fcd72066
HEY1MIK <kwon5943@gmail.com>
2022-08-26 16:49:06 +0900

    초기 프로젝트

---
+++ inviteU/src/main/java/com/sample/invite/InviteUApplication.java
@@ -0,0 +1,13 @@
+package com.sample.invite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InviteUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InviteUApplication.class, args);
+	}
+
+}
---
+++ inviteU/src/test/java/com/sample/invite/InviteUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.invite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InviteUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
